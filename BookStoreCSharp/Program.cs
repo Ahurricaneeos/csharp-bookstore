e065aa42be13a4a0a0b295f40db7cd465e009438
Ahurricaneeos <aurora_sala@hotmail.it>
2023-04-12 14:50:52 +0200

    Exercise complete

--- BookStoreCSharp/Program.cs
+++ BookStoreCSharp/Program.cs
@@ -1,1 +1,43 @@
-﻿
+﻿// Dichiarazione variabili:
+
+string title = ("Clean Code");
+string author = ("Robert C. “Uncle Bob” Martin");
+string isbn = ("9780132350884");
+int pages = 431;
+int weight = 800;
+float lenght = 17.78f;
+float height = 23.37f;
+float depht = 2.79f;
+float rating = 4.7f;
+int reviews = 5413;
+bool kindle = true;
+bool flexibleCover = true;
+
+if (!kindle)
+{
+    Console.WriteLine("No");
+} else
+{
+    Console.WriteLine("Si");
+}
+
+if (flexibleCover)
+{
+    Console.WriteLine("Si");
+}
+
+// Stampa delle informazioni:
+
+Console.WriteLine("-------IL LIBRO DI OGGI: " +  title + " di " + author + " -------");
+Console.WriteLine("Informazioni generiche:");
+Console.WriteLine("ISBN: " + isbn);
+Console.WriteLine("Numero delle pagine: " +  pages + " pagine");
+Console.WriteLine("Peso del libro: " +  weight + "kg");
+Console.WriteLine("Dimensioni del libro: " + lenght + "cm x " + height + "cm x " + depht + "cm");
+Console.WriteLine(" ");
+Console.WriteLine("Informazioni Amazon:");
+Console.WriteLine("Numero di recensioni: " + reviews + " recensioni");
+Console.WriteLine("Valutazione media: " + rating + " stelline");
+Console.WriteLine("Kindle disponibile: " + kindle);
+Console.WriteLine("Copertina flessibile disponibile: " + flexibleCover);
+Console.WriteLine("---------------------------------------------------------------------------");
